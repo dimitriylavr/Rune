4b4d7155feaf122fcbd1bbe50ea98f0ca7f0044f
Dmitry <ldimka@gmail.com>
2021-11-25 23:24:03 +0300

    Create runeList.dart

---
+++ rune_app/lib/data/runeList.dart
@@ -0,0 +1,10 @@
+List<dynamic> runeList() {
+  return [
+    {
+      'title': 'Тестовый заголовок',
+      'image':
+          'https://www.moviepostersgallery.com/wp-content/uploads/2020/08/Blackwidow2.jpg',
+      'description': 'Описание будет таким'
+    },
+  ];
+}
