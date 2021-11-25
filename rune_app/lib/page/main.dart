638d7631fab529d925243d0cbbe017aecacc04f6
Dmitry <ldimka@gmail.com>
2021-11-25 22:48:03 +0300

    Update main.dart

--- rune_app/lib/page/main.dart
+++ rune_app/lib/page/main.dart
@@ -10,10 +10,15 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   CarouselController _carouselController = new CarouselController();
-  int current = 0;
+  int _current = 0;
 
-  List<dynamic> _runes = [
-    {'title': 'Test title', 'image': '', 'description': 'Описание такое'}
+  final List<dynamic> _runes = [
+    {
+      'title': 'Тестовый заголовок',
+      'image':
+          'https://www.moviepostersgallery.com/wp-content/uploads/2020/08/Blackwidow2.jpg',
+      'description': 'Описание будет таким'
+    },
   ];
 
   @override
@@ -23,6 +28,180 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Image.network(
+              _runes[_current]['image'],
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [
+                        Colors.grey.shade50.withOpacity(1),
+                        Colors.grey.shade50.withOpacity(1),
+                        Colors.grey.shade50.withOpacity(1),
+                        Colors.grey.shade50.withOpacity(1),
+                        Colors.grey.shade50.withOpacity(0.0),
+                        Colors.grey.shade50.withOpacity(0.0),
+                        Colors.grey.shade50.withOpacity(0.0),
+                        Colors.grey.shade50.withOpacity(0.0),
+                      ]),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 50,
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 500,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.70,
+                  enableInfiniteScroll: true,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  },
+                ),
+                carouselController: _carouselController,
+                items: _runes.map((rune) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white60,
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: 320,
+                                  margin: const EdgeInsets.only(top: 30),
+                                  clipBehavior: Clip.hardEdge,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Image.network(rune['image'],
+                                      fit: BoxFit.cover),
+                                ),
+                                const SizedBox(height: 20),
+                                Text(
+                                  rune['title'],
+                                  style: const TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                // rating
+                                const SizedBox(height: 20),
+                                Container(
+                                  child: Text(
+                                    rune['description'],
+                                    style: TextStyle(
+                                        fontSize: 14.0,
+                                        color: Colors.grey.shade600),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                AnimatedOpacity(
+                                  duration: const Duration(milliseconds: 500),
+                                  opacity: _current == _runes.indexOf(rune)
+                                      ? 1.0
+                                      : 0.0,
+                                  child: Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 20.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            const Icon(
+                                              Icons.star,
+                                              color: Colors.yellow,
+                                              size: 20,
+                                            ),
+                                            const SizedBox(width: 5),
+                                            Text(
+                                              '4.5',
+                                              style: TextStyle(
+                                                  fontSize: 14.0,
+                                                  color: Colors.grey.shade600),
+                                            )
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.access_time,
+                                              color: Colors.grey.shade600,
+                                              size: 20,
+                                            ),
+                                            const SizedBox(width: 5),
+                                            Text(
+                                              '2h',
+                                              style: TextStyle(
+                                                  fontSize: 14.0,
+                                                  color: Colors.grey.shade600),
+                                            )
+                                          ],
+                                        ),
+                                        Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.2,
+                                          child: Row(
+                                            children: [
+                                              Icon(
+                                                Icons.play_circle_filled,
+                                                color: Colors.grey.shade600,
+                                                size: 20,
+                                              ),
+                                              const SizedBox(width: 5),
+                                              Text(
+                                                'Watch',
+                                                style: TextStyle(
+                                                    fontSize: 14.0,
+                                                    color:
+                                                        Colors.grey.shade600),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ));
+                    },
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
