a7bd7bbee5da1fb9a0577f8333ce6e1eca1a4975
Dmitry <ldimka@gmail.com>
2021-11-26 17:48:26 +0300

    Update rune_list.dart

--- rune_app/lib/data/rune_list.dart
+++ rune_app/lib/data/rune_list.dart
@@ -1,31 +1,145 @@
 List<dynamic> runeList() {
   return [
     {
-      'title': 'Тестовый заголовок',
-      'image':
-          'https://www.moviepostersgallery.com/wp-content/uploads/2020/08/Blackwidow2.jpg',
-      'description': 'Описание будет таким'
+      'title': 'Фэху',
+      'image': 'assets/images/runes/fehu.png',
+      'description':
+          'Руна Фэху связана с богатством и применяется для притяжения материальных благ.'
     },
     {
-      'title': 'Второй заголовок',
-      'image':
-          'https://www.moviepostersgallery.com/wp-content/uploads/2021/11/Mondohalloween17.jpg',
+      'title': 'Уруз',
+      'image': 'assets/images/runes/uruz.png',
       'description':
-          'Описание можно сделать чуть побольше. Не знаю зачем, но надо!'
+          'Руна Уруз соединяет миры Альфхейм и Муспельхейм Древа Иггдрасиль.'
     },
     {
-      'title': 'И третий заголовок',
-      'image':
-          'https://www.moviepostersgallery.com/wp-content/uploads/2021/10/Venom20211.jpg',
+      'title': 'Турисаз',
+      'image': 'assets/images/runes/Thurisaz.png',
       'description':
-          'Мождно даже ещё чуть больше чем предыдущий. Описание можно сделать чуть побольше. Не знаю зачем, но надо!'
+          'Архетипически руна Турисаз  — это молот бога Тора Мьельнир, то есть освобождение, разрушение'
     },
     {
-      'title': 'Четвёртый почётный!',
-      'image':
-          'https://www.moviepostersgallery.com/wp-content/uploads/2021/09/Lolita16.jpg',
+      'title': 'Анзус',
+      'image': 'assets/images/runes/Ansuz.png',
       'description':
-          'Мождно даже ещё чуть больше чем предыдущий. Описание можно сделать чуть побольше. Не знаю зачем, но надо!'
+          'Это руна Одина, архетипически  — руна божественного вдохновения. На языке этой руны говорят боги, эгрегоры, архетипы. Если она встречается в мантике, это значит, что боги наблюдают за гадающим.'
+    },
+    {
+      'title': 'Райдо',
+      'image': 'assets/images/runes/Raidho.png',
+      'description':
+          'Руна соединяет миры Альфхейм и  Нифельхейм Древа Иггдрасиль.'
+    },
+    {
+      'title': 'Кено',
+      'image': 'assets/images/runes/Kenaz.png',
+      'description':
+          'Архетипически Кено означает «факел», свет. Люди, проводящие эту руну, обладают повышенной харизмой, внутренним «огнем», притягивающим других людей.'
+    },
+    {
+      'title': 'Гебо',
+      'image': 'assets/images/runes/Gebo.png',
+      'description':
+          'Гебо  — «дар Вселенной», руна, имеющая тройственную природу.'
+    },
+    {
+      'title': 'Винью',
+      'image': 'assets/images/runes/Wunjo.png',
+      'description':
+          'Винью - руна радости, общности и удовольствия. Вестник хороших новостей; обещает будущее счастье и межличностную гармонию.'
+    },
+    {
+      'title': 'Хагалаз',
+      'image': 'assets/images/runes/Hagalaz.png',
+      'description':
+          'Хагалаз — это главная боевая руна, одна из двух «темных» (колдовских) рун Футарка.'
+    },
+    {
+      'title': 'Наутиз',
+      'image': 'assets/images/runes/Nauthiz.png',
+      'description': 'Руна соединяет миры Альфхейм и Мидгард Древа Иггдрасиль.'
+    },
+    {
+      'title': 'Иса',
+      'image': 'assets/images/runes/Isa.png',
+      'description':
+          'Все страсти, вещи \nи обстоятельства —\nЭто просто атрибуты Жизни.\n Сама Жизнь — в тебе'
+    },
+    {
+      'title': 'Джера',
+      'image': 'assets/images/runes/Jera.png',
+      'description':
+          'Все течет, все изменяется.\nНеизбежны перемены.\nЯ управляю изменениями\nВ своем теле.'
+    },
+    {
+      'title': 'Эйваз',
+      'image': 'assets/images/runes/Eihwaz.png',
+      'description':
+          'То, что происходит сейчас,\nУже было.\nТо, что есть, — будет потом.'
+    },
+    {
+      'title': 'Перта',
+      'image': 'assets/images/runes/Perthro.png',
+      'description': 'Мир, Я чаша,\nНаполненная твоей любовью\nИ твоим светом'
+    },
+    {
+      'title': 'Альгиз',
+      'image': 'assets/images/runes/Algiz.png',
+      'description':
+          'Смири злобные сердца врагов моих\nИ сокруши их, как прах\nПеред лицом ветра.'
+    },
+    {
+      'title': 'Соулу',
+      'image': 'assets/images/runes/Sowilo.png',
+      'description':
+          'В тебе солнце.\nИ его лучи идут туда,\nКуда ты захочешь.\nЯ — свет, я — истина.'
+    },
+    {
+      'title': 'Тейваз',
+      'image': 'assets/images/runes/Tiwaz.png',
+      'description':
+          'Я рожден,\nЧтобы побеждать и преуспевать.\nЯ рожден для побед.'
+    },
+    {
+      'title': 'Беркана',
+      'image': 'assets/images/runes/Berkano.png',
+      'description':
+          'В омут я ныряю,\nВодоворот затягивает меня.\nЯ наполняюсь энергией.\nЭнергия проходит через мое тело.'
+    },
+    {
+      'title': 'Эваз',
+      'image': 'assets/images/runes/Ehwaz.png',
+      'description':
+          'Я вижу Коня.\nЯ скачу на Коне.\nМир меняется.\nЯ прохожу между мирами.'
+    },
+    {
+      'title': 'Манназ',
+      'image': 'assets/images/runes/Mannaz.png',
+      'description':
+          'Мой Бог — это мой жизненный принцип,\nЭто мой Дух и разум.'
+    },
+    {
+      'title': 'Лагуз',
+      'image': 'assets/images/runes/Laguz.png',
+      'description':
+          'Я стою в потоке воды.\nЯ вижу родник.\nВнутреннее знание приходит ко мне.\nЯ общаюсь с Богами.'
+    },
+    {
+      'title': 'Ингуз',
+      'image': 'assets/images/runes/Inguz.png',
+      'description':
+          'Огонь горит внутри меня.\nЯ наполняюсь внутренним светом,\nЯ расту,\nЯ излучаю силу.'
+    },
+    {
+      'title': 'Дагаз',
+      'image': 'assets/images/runes/Dagaz.png',
+      'description': 'Я вперед на сто шагов\nИ от друзей, и от врагов.'
+    },
+    {
+      'title': 'Отила',
+      'image': 'assets/images/runes/Othala.png',
+      'description':
+          'Стоит сундук,\nЯ открываю его.\nВ нем лежит книга,\nЯ читаю ее.\nЯ включаюсь в Наследие.'
     },
   ];
 }
