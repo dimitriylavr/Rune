7826567f31bab6872d78c9b62f6533e528710feb
Dmitry <ldimka@gmail.com>
2021-11-29 17:06:23 +0300

    Create rune_class.dart

---
+++ rune_apps/lib/data/rune_class.dart
@@ -0,0 +1,12 @@
+class Rune {
+  final String title;
+  final String image;
+  final String description;
+  final String practic;
+
+  Rune(
+      {required this.title,
+      required this.image,
+      required this.description,
+      required this.practic});
+}
