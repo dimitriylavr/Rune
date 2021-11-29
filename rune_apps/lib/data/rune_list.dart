7040464cd8e6c2e19eaeead3af1273580ba70969
Dmitry <ldimka@gmail.com>
2021-11-29 17:06:25 +0300

    Update rune_list.dart

--- rune_apps/lib/data/rune_list.dart
+++ rune_apps/lib/data/rune_list.dart
@@ -3,141 +3,165 @@ List<dynamic> runeList() {
     {
       'title': 'Фэху',
       'image': 'assets/images/runes/fehu.png',
+      'practic': '',
       'description':
           'Я источник моих потребностей.\nЯ источник моих поставок.\nВсе мои потребности удовлетворяются\nВ каждый момент времени,\nВ каждой точке пространства.'
     },
     {
       'title': 'Уруз',
       'image': 'assets/images/runes/uruz.png',
+      'practic': '',
       'description':
           'Животные силы пробуждаются во мне.\nЯ — Аурох.\nЯ чувствую мощный поток энергии.\nМое тело превращается в сгусток энергии.'
     },
     {
       'title': 'Турисаз',
       'image': 'assets/images/runes/Thurisaz.png',
+      'practic': '',
       'description': 'У меня есть все препятствия,\nНеобходимые для успеха.'
     },
     {
       'title': 'Анзус',
       'image': 'assets/images/runes/Ansuz.png',
+      'practic': '',
       'description':
           'Мой бог — это мой жизненный принцип,\nМой дух, мой разум и моя мысль.'
     },
     {
       'title': 'Райдо',
       'image': 'assets/images/runes/Raidho.png',
+      'practic': '',
       'description':
           'Крутится Колесо Времени,\nВсе повторяется,\nВсе возвращается.\nЯ знаю свое предназначение,\nЯ двигаюсь к цели.'
     },
     {
       'title': 'Кено',
       'image': 'assets/images/runes/Kenaz.png',
+      'practic': '',
       'description':
           'Я вижу Огонь.\nЯ стою на огненной пирамиде.\nОгонь проходит через мое тело.\nЯ могу лечить.'
     },
     {
       'title': 'Гебо',
       'image': 'assets/images/runes/Gebo.png',
+      'practic': '',
       'description':
           'Мое сознание расширяется.\nЯ чувствую людей.\nЯ читаю их мысли.\nЯ могу управлять ими.'
     },
     {
       'title': 'Винью',
       'image': 'assets/images/runes/Wunjo.png',
+      'practic': '',
       'description':
           'Я — огненный цветок.\nПодобно пламени свечи,\nПритягиваю я людей.\nЯ слаще меда.'
     },
     {
       'title': 'Хагалаз',
       'image': 'assets/images/runes/Hagalaz.png',
+      'practic': '',
       'description':
           'Я вижу радугу.\nХеймдалль стережет ее.\nБоги защищают и охраняют меня.\nЯ очищаю свое сознание.\nЯ иду в мир Богов.'
     },
     {
       'title': 'Наутиз',
       'image': 'assets/images/runes/Nauthiz.png',
+      'practic': '',
       'description':
           'Ты свобода,\nИдущая за преодолением\nВнутреннего напряжения.'
     },
     {
       'title': 'Иса',
       'image': 'assets/images/runes/Isa.png',
+      'practic': '',
       'description':
           'Все страсти, вещи \nи обстоятельства —\nЭто просто атрибуты Жизни.\n Сама Жизнь — в тебе'
     },
     {
       'title': 'Джера',
       'image': 'assets/images/runes/Jera.png',
+      'practic': '',
       'description':
           'Все течет, все изменяется.\nНеизбежны перемены.\nЯ управляю изменениями\nВ своем теле.'
     },
     {
       'title': 'Эйваз',
       'image': 'assets/images/runes/Eihwaz.png',
+      'practic': '',
       'description':
           'То, что происходит сейчас,\nУже было.\nТо, что есть, — будет потом.'
     },
     {
       'title': 'Перта',
       'image': 'assets/images/runes/Perthro.png',
+      'practic': '',
       'description': 'Мир, Я чаша,\nНаполненная твоей любовью\nИ твоим светом'
     },
     {
       'title': 'Альгиз',
       'image': 'assets/images/runes/Algiz.png',
+      'practic': '',
       'description':
           'Смири злобные сердца врагов моих\nИ сокруши их, как прах\nПеред лицом ветра.'
     },
     {
       'title': 'Соулу',
       'image': 'assets/images/runes/Sowilo.png',
+      'practic': '',
       'description':
           'В тебе солнце.\nИ его лучи идут туда,\nКуда ты захочешь.\nЯ — свет, я — истина.'
     },
     {
       'title': 'Тейваз',
       'image': 'assets/images/runes/Tiwaz.png',
+      'practic': '',
       'description':
           'Я рожден,\nЧтобы побеждать и преуспевать.\nЯ рожден для побед.'
     },
     {
       'title': 'Беркана',
       'image': 'assets/images/runes/Berkano.png',
+      'practic': '',
       'description':
           'В омут я ныряю,\nВодоворот затягивает меня.\nЯ наполняюсь энергией.\nЭнергия проходит через мое тело.'
     },
     {
       'title': 'Эваз',
       'image': 'assets/images/runes/Ehwaz.png',
+      'practic': '',
       'description':
           'Я вижу Коня.\nЯ скачу на Коне.\nМир меняется.\nЯ прохожу между мирами.'
     },
     {
       'title': 'Манназ',
       'image': 'assets/images/runes/Mannaz.png',
+      'practic': '',
       'description':
           'Мой Бог — это мой жизненный принцип,\nЭто мой Дух и разум.'
     },
     {
       'title': 'Лагуз',
       'image': 'assets/images/runes/Laguz.png',
+      'practic': '',
       'description':
           'Я стою в потоке воды.\nЯ вижу родник.\nВнутреннее знание приходит ко мне.\nЯ общаюсь с Богами.'
     },
     {
       'title': 'Ингуз',
       'image': 'assets/images/runes/Inguz.png',
+      'practic': '',
       'description':
           'Огонь горит внутри меня.\nЯ наполняюсь внутренним светом,\nЯ расту,\nЯ излучаю силу.'
     },
     {
       'title': 'Дагаз',
       'image': 'assets/images/runes/Dagaz.png',
+      'practic': '',
       'description': 'Я вперед на сто шагов\nИ от друзей, и от врагов.'
     },
     {
       'title': 'Отила',
       'image': 'assets/images/runes/Othala.png',
+      'practic': '',
       'description':
           'Стоит сундук,\nЯ открываю его.\nВ нем лежит книга,\nЯ читаю ее.\nЯ включаюсь в Наследие.'
     },
